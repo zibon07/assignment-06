51da6d5b7b9a7da008df307dc3f83db54d4dad69
zibon07 <shorifhussainzibon@gmail.com>
2026-01-06 19:49:37 +0600

    all trees showed in js

--- script/index.js
+++ script/index.js
@@ -6,28 +6,39 @@ const loadCategories = () => {
         .then((res) => res.json())
         .then((json) => displayCategories(json.categories))
 }
-
 const displayCategories = (categories) => {
-    // console.log(categories)
+
     // 1.get the container & empty it
+
     const categoryContainer = document.getElementById("category-container");
-    // categoryContainer.innerHTML = " ";
 
     // 2.get into every category
+
     for (let i = 0; i < categories.length; i++) {
-        // console.log(categories[i].category_name)
 
         // 3.create element
+
         const categoryList = document.createElement("div");
         categoryList.innerHTML = `
-                    <div class="category-list mt-3 text-left">
-                        <button class="text-lg font-medium w-full justify-start text-left rounded-lg p-2 hover:bg-[#15803D] hover:text-white">${categories[i].category_name}</button>
+                    <div class="category-list mt-2 text-left">
+                        <button class="text-lg font-medium w-full justify-start text-left rounded-md px-2 hover:bg-[#15803D] hover:text-white">${categories[i].category_name}</button>
                     </div>
-
         `
         categoryContainer.append(categoryList)
 
     }
 
 }
+const loadAllTrees = () => {
+    fetch("https://openapi.programming-hero.com/api/plants")
+        .then((res) => res.json())
+        .then((json) => displayAllTrees(json.plants))
+}
+
+const displayAllTrees = (allTrees) => {
+    console.log(allTrees)
+}
+
+
+loadAllTrees()
 loadCategories()
